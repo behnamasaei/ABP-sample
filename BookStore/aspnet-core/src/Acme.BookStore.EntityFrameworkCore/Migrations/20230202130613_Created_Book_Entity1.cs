76bef866de445c8c37ef50dfc5761235b4bab1a0
BehnamAsaei <behnamasaei@gmail.com>
2023-02-02 16:49:59 +0330

    update connection string

---
+++ BookStore/aspnet-core/src/Acme.BookStore.EntityFrameworkCore/Migrations/20230202130613_Created_Book_Entity1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acme.BookStore.Migrations
+{
+    public partial class Created_Book_Entity1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublisthDate",
+                table: "AppBooks",
+                newName: "PublishDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PublishDate",
+                table: "AppBooks",
+                newName: "PublisthDate");
+        }
+    }
+}
