f356217f3c6e05f035cef985c0e5edc6f431a720
BehnamAsaei <behnamasaei@gmail.com>
2023-02-02 16:07:51 +0330

    implement book service

---
+++ BookStore/aspnet-core/src/Acme.BookStore.Application/BookAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Acme.BookStore.Books;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore;
+
+public class BookAppService : CrudAppService<Books.Book,BookDto,Guid,PagedAndSortedResultRequestDto,CreateUpdateBookDto>,
+    IBookAppService
+{
+    public BookAppService(IRepository<Books.Book,Guid> repository) : base(repository)
+    {
+        
+    }
+}
